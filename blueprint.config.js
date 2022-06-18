aae18580f1e75cc12ec45e538b14ad23dbd63d0f
Cory Rylan <cory@coryrylan.com>
2022-06-18 09:36:12 -0500

    chore: migrate old repo in org for layout branch
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

---
+++ blueprint.config.js
@@ -0,0 +1,45 @@
+
+export default { 
+  library: {
+    externals: [],
+  },
+  drafter: {
+    dist: './dist/drafter',
+    examples: './src/**/*.examples.js',
+    aliases: [
+      { find: /^@blueprintui\/layout\/(.+)\.css$/, replacement: `./dist/lib/$1.css` },
+    ],
+    head: () => {
+      return /* html */`
+        <script type="module">
+          import 'modern-normalize/modern-normalize.css';
+          import '@blueprintui/tokens/index.css';
+          import '@blueprintui/layout/index.css';
+        </script>
+        <style>
+          body {
+            padding: 24px;
+            margin: 0;
+          }
+
+          [bp-layout] > div:not([bp-layout~='inline']):not([bp-layout~='block']):not([bp-layout~='grid']) {
+            background: #f4f4f4;
+            min-width: 60px;
+            min-height: 60px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+
+          [demo] {
+            border: 1px solid #e6e6e6;
+          }
+
+          hr {
+            border-color: #e6e6e6;
+          }
+        </style>
+      `;
+    }
+  }
+};
