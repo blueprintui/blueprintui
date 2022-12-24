47f1e3cfeceb514c2be6dfffb41ef39e04c81a94
Cory Rylan <cory@coryrylan.com>
2022-12-24 16:56:56 -0600

    chore: improve icon docs
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

--- packages/icons/src/internals/event-subject.ts
+++ packages/icons/src/internals/event-subject.ts
@@ -3,6 +3,7 @@ import { createId } from './utils.js';
 export class BroadcastSubscription {
   #channel: BroadcastChannel;
   constructor(public fn: (value: any) => void, channelId: string) {
+    // https://stackoverflow.com/questions/67041534/why-create-multiple-instance-of-same-broadcastchannel-lead-to-browser-freeze
     this.#channel = new BroadcastChannel(channelId);
     this.#channel.addEventListener('message', e => fn(e.data));
   }
