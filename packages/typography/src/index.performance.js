7b037a9c65e36c7403bbcfe62dbf2e02360549a5
Cory Rylan <cory@coryrylan.com>
2023-01-08 17:20:31 -0600

    fix(typography): link inherit defaults
    - allow link option to inherit font weight and decoration
    
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

--- packages/typography/src/index.performance.js
+++ packages/typography/src/index.performance.js
@@ -1,7 +1,7 @@
 import { testBundleSize } from 'web-test-runner-performance/browser.js';
 
 describe('performance', () => {
-  it('should build under 1kb', async () => {
-    expect((await testBundleSize(`import './dist/lib/index.min.css'`, { optimize: true })).kb).toBeLessThan(1);
+  it('should build under 1.1kb', async () => {
+    expect((await testBundleSize(`import './dist/lib/index.min.css'`, { optimize: true })).kb).toBeLessThan(1.1);
   });
 });
