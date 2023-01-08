f3150ad75c6484d8d9cb20b33ba20b5b0facff71
Cory Rylan <cory@coryrylan.com>
2023-01-08 15:00:04 -0600

    fix(build): release path
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

--- .releaserc.js
+++ .releaserc.js
@@ -15,6 +15,12 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
+    [
       "@semantic-release/git",
       {
         "assets": ["CHANGELOG.md"]
