6a9af1f06c862eb53d0165733957245d4d1112ca
Cory Rylan <cory@coryrylan.com>
2023-01-08 14:28:24 -0600

    fix(build): changelog
    - fix changelog generation
    
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

--- .releaserc.js
+++ .releaserc.js
@@ -15,12 +15,6 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     [
-      "@semantic-release/changelog",
-      {
-        "changelogFile": "CHANGELOG.md"
-      }
-    ],
-    [
       "@semantic-release/git",
       {
         "assets": ["CHANGELOG.md"]
