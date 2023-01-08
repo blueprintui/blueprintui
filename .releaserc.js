fe4b6b108624a5fcd9fc58ac856ad02a5de19c21
Cory Rylan <cory@coryrylan.com>
2023-01-07 23:21:38 -0600

    chore: cd setup
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

---
+++ .releaserc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          { breaking: true, release: 'major' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'chore', release: false }
+        ]
+      }
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    [
+      '@amanda-mitchell/semantic-release-npm-multiple',
+      {
+        registries: {
+          components: {
+            npmPublish: true,
+            pkgRoot: './packages/components/dist/lib'
+          },
+          grid: {
+            npmPublish: true,
+            pkgRoot: './packages/grid/dist/lib'
+          },
+          icons: {
+            npmPublish: true,
+            pkgRoot: './packages/icons/dist/lib'
+          },
+          layout: {
+            npmPublish: true,
+            pkgRoot: './packages/layout/dist/lib'
+          },
+          themes: {
+            npmPublish: true,
+            pkgRoot: './packages/themes/dist/lib'
+          },
+          typewriter: {
+            npmPublish: true,
+            pkgRoot: './packages/typewriter/dist/lib'
+          },
+          typogrpahy: {
+            npmPublish: true,
+            pkgRoot: './packages/typogrpahy/dist/lib'
+          }
+        }
+      }
+    ]
+  ]
+};
