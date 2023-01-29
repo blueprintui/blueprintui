59aec4ffb32d0bc89272cea7a9d82acb4a81e5b8
Cory Rylan <cory@coryrylan.com>
2023-01-29 16:59:00 -0600

    fix(crane): minor cleanup in dom fns
    - minor fn cleanup
    - test ci deploy
    
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

--- packages/crane/src/internals/utils/dom.ts
+++ packages/crane/src/internals/utils/dom.ts
@@ -1,11 +1,9 @@
 
 export function removeAttributeValue(el: HTMLElement, attr: string, value: string) {
-  const values = el.getAttribute(attr)?.split(' ') || [];
-  const attrValue = values.filter(v => v !== value).join(' ');
+  const attrValue = (el.getAttribute(attr)?.split(' ') || []).filter(v => v !== value).join(' ');
   attrValue.length ? el.setAttribute(attr, attrValue) : el.removeAttribute(attr);
 }
 
 export function addAttributeValue(el: HTMLElement, attr: string, value: string) {
-  const values = new Set([...(el.getAttribute(attr)?.split(' ') || []), value.trim()]);
-  el.setAttribute(attr, [...values].join(' ').trim());
+  el.setAttribute(attr, [...new Set([...(el.getAttribute(attr)?.split(' ') || []), value.trim()])].join(' ').trim());
 }
