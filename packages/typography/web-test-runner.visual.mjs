f2e7df37bc79ccba0cf296e0569e0f0e3a02f3a4
Cory Rylan <cory@coryrylan.com>
2023-04-16 14:24:27 -0500

    chore(build): type visual tests
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

---
+++ packages/typography/web-test-runner.visual.mjs
@@ -0,0 +1,35 @@
+import { visualRegressionPlugin } from '@web/test-runner-visual-regression/plugin';
+import { playwrightLauncher } from '@web/test-runner-playwright';
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { fromRollup } from '@web/dev-server-rollup';
+import { jasmineTestRunnerConfig } from 'web-test-runner-jasmine';
+import execute from 'rollup-plugin-shell';
+
+export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
+  ...jasmineTestRunnerConfig(),
+  testFramework: {
+    config: {
+      styles: [
+        '../../node_modules/@blueprintui/themes/index.min.css',
+        '../../node_modules/@blueprintui/themes/modern/index.min.css',
+        '../../node_modules/@blueprintui/themes/modern-dark/index.min.css',
+        '../../node_modules/@blueprintui/typography/index.min.css',
+        '../../node_modules/@blueprintui/layout/index.min.css'
+      ]
+    }
+  },
+  files: ['./src/**/*.visual.js'],
+  browsers: [playwrightLauncher({ product: 'chromium' })],
+  nodeResolve: true,
+  dedupe: true,
+  plugins: [
+    // ...baseConfig.plugins,
+    // https://github.com/evanw/esbuild/issues/2220#issuecomment-1116082001
+    esbuildPlugin({ ts: true, target: 'es2020' }),
+    fromRollup(execute)({ commands: [`tsc --noEmit src/**/*.visual.js`], hook: 'writeBundle' }),
+    visualRegressionPlugin({
+      update: process.argv.includes('--update-visual-baseline'),
+      failureThreshold: 5
+    })
+  ]
+});
