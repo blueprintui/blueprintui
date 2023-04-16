749a72c400668e7f936e225665ee525f7ab0c20f
Cory Rylan <cory@coryrylan.com>
2023-04-16 13:41:27 -0500

    chore(build): add layout visual tests
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

---
+++ packages/layout/src/inline.visual.js
@@ -0,0 +1,25 @@
+import { html } from 'lit';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import { createVisualFixture, removeFixture } from '@blueprintui/components/test';
+import { styles } from './test.js';
+import * as inline from './inline.examples.js';
+
+describe('inline', () => {
+  let fixture;
+
+  beforeEach(async () => {
+    fixture = await createVisualFixture(html`
+      ${styles} ${unsafeHTML(inline.example())} ${unsafeHTML(inline.wrap())} ${unsafeHTML(inline.gap())}
+      ${unsafeHTML(inline.alignment())} ${unsafeHTML(inline.itemAlignment())} ${unsafeHTML(inline.stretch())}
+    `);
+  });
+
+  afterEach(() => {
+    removeFixture(fixture);
+  });
+
+  it('inline layout', async () => {
+    await visualDiff(fixture, 'inline.png');
+  });
+});
