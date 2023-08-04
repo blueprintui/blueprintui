7e2db6d33e33ca7601c7d32d46cd425d1725d0f7
Cory Rylan <cory@coryrylan.com>
2023-08-03 23:16:33 -0500

    fix(components): upstream package types
    - fix type with upstream dependency types
    
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

--- packages/components/src/internals/utils/animation.ts
+++ packages/components/src/internals/utils/animation.ts
@@ -1,6 +1,7 @@
 import { animate } from '@lit-labs/motion';
+import type { DirectiveResult } from 'lit/directive.js';
 
-export function fade(element: HTMLElement) {
+export function fade(element: HTMLElement): DirectiveResult {
   const style = getComputedStyle(element);
   const easing = style.getPropertyValue('--animation-easing') || 'ease-in-out';
   const duration = parseInt(style.getPropertyValue('--animation-duration') || '200');
