7860cff01ebccd7186679a7e8571302b5557e099
Cory Rylan <cory@coryrylan.com>
2023-10-11 22:19:57 -0500

    fix(build): commit lint rule
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -30,6 +30,7 @@ module.exports = {
     'subject-max-length': [2, 'always', 100],
     'body-max-line-length': [0, 'always', 100],
     'body-max-length': [0, 'always', 100],
+    'footer-max-line-length': [0, 'always', 100],
     'type-empty': [2, 'never'],
     'type-enum': [2, 'always', ['chore', 'feat', 'fix']]
   }
