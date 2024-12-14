cca1e424ab3abcffaa3f5c3973ba4edc70422f25
Cory Rylan <cory@coryrylan.com>
2024-12-13 18:30:24 -0700

    chore(examples): angular update
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 import '@blueprintui/components/include/alert.js';
 import '@blueprintui/components/include/button.js';
 
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule],
+  imports: [RouterOutlet],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   showAlert = true;
