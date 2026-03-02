0e38a0b5866ef346dd55f53882034f748460ccde
Cory Rylan <cory@coryrylan.com>
2026-03-01 21:52:42 -0600

    fix(orbit): event listener cleanup
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

--- projects/orbit/src/index.ts
+++ projects/orbit/src/index.ts
@@ -1,5 +1,1 @@
-// export * from './popover/index.js';
-// export * from './anchor/index.js';
-// export * from './scroll-lock/index.js';
-// export * from './focus/index.js';
 export * from './popover/popover.mixin.js';
--- projects/orbit/src/internals/scroll-lock.spec.ts
+++ projects/orbit/src/internals/scroll-lock.spec.ts
@@ -3,12 +3,10 @@ import { enableScrollLock, disableScrollLock } from './scroll-lock.js';
 describe('scroll-lock', () => {
   afterEach(() => {
     disableScrollLock();
-    document.documentElement.style.scrollbarGutter = '';
   });
 
   it('should enable scroll lock', () => {
     enableScrollLock();
-    expect(document.documentElement.style.scrollbarGutter).toBe('stable');
     expect(document.body.style.overflow).toBe('hidden');
   });
 
